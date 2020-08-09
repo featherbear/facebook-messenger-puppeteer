68c086ee9ee904d230e736597afa10b8058151d1
Andrew Wong <andrew.j.wong@outlook.com>
2020-08-10 03:30:55 +1000

    Add driver code

---
+++ run.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+
+const Client = require('./api')
+
+const credentials = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'config.json'), 'utf8')
+)
+credentials.session = fs.existsSync(path.join(__dirname, '.appstate.json'))
+  ? JSON.parse(fs.readFileSync(path.join(__dirname, '.appstate.json'), 'utf8'))
+  : null
+
+const api = new Client()
+
+;(async () => {
+  await api.login(credentials)
+
+  // Save current cookie
+  fs.writeFileSync(
+    path.join(__dirname, '.appstate.json'),
+    JSON.stringify(await api.getSession())
+  )
+
+  api.listen(json => {
+    // json.class === 'NewMessage'
+    // json.body
+    // json.messageMetadata.actorFbId
+    // json.messageMetadata.threadKey.threadFbId
+    // json.messageMetadata.messageId
+    // json.messageMetadata.timestamp
+
+    console.log(json)
+  })
+})()
